d28a5aac4d151435b24547819ebae8c3a4b9c339
klanti islam <klantyislam@yahoo.com>
2023-05-18 20:09:23 -0400

    tests for initial state of app

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('initial state of the app', () => {
+// the app starts off with a blue button with text "change color to red"
+  render(<App/>)
+  const colorBtn = screen.getByRole('button', {name:/change color to red/i})
+  expect(colorBtn).toHaveStyle({backgroundColor:'blue'})
 
 });
