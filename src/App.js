99c7ad5d7d88d857a417241e30565080f6439489
klanti islam <klantyislam@yahoo.com>
2023-05-18 20:36:06 -0400

    added code for onClick functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [color, setColor] = useState('blue')
+  const btnColor = color ==='red'?'blue':'red'
   return (
     <div className="App">
-      <button style={{backgroundColor:'blue'}}>
-        Change color to red</button>
+      <button style={{backgroundColor:color}}
+      onClick={()=>{setColor(btnColor)}}>
+        Change color to {btnColor}</button>
     </div>
   );
 }
